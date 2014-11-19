204bf6cf6855d3617d129ee00ea1b6eaea6cd5d1
marius <marius.kriegerowski@zmaw.de>
2014-11-19 22:20:15 +0100

    improved use case

--- synthi.py
+++ synthi.py
@@ -1,38 +1,52 @@
-from pyrocko.gf.seismosizer import RemoteEngine, Target
+from pyrocko.gf.seismosizer import RemoteEngine, Target, LocalEngine
+from visualizer import Visualizer
+from source_region import *
 import numpy as num
 
-# We need a pyrocko.gf.seismosizer.Engine object which provides us with the 
-# traces extracted from the store. In this case we are going to use a local 
-# engine since we are going to query a local store.
-engine = LocalEngine(store_superdirs=['/data/stores'])
 
-# The store we are going extract data from:
-store_id = 'crust2_dd'
+if __name__=='__main__':
 
-# Define a list of pyrocko.gf.seismosizer.Target objects, representing the 
-# recording devices. In this case one station with a three component sensor 
-# will serve fine for demonstation. 
-channel_codes = 'ENZ'
-targets = [Target(lat=10., 
-                  lon=10., 
-                  store_id=store_id,
-                  codes=('', 'STA', '', channel_code))
-                        for channel_code in channel_codes]
+    number_sources = 100
+    geometry = RectangularSourceGeometry(center_lon=10, 
+                                     center_lat=10.,
+                                     center_depth=8000,
+                                     azimuth=40.,
+                                     dip=40.,
+                                     tilt=45.,
+                                     length=6000.,
+                                     depth=6000.,
+                                     thickness=500., 
+                                     n=number_sources)
+    
+    base_source = seismosizer.DCSource(lat=0, lon=0, depth=0, 
+                                       strike=170, dip=80,rake=30)
+    
+    timing = RandomTiming(tmin=1000, tmax=100000, number=number_sources)
+    
+    mechanisms = FocalDistribution(n=number_sources, 
+                                   base_source=base_source, 
+                                   variation=20)
 
-# Let's use a double couple source representation.
-source_dc = DCSource(lat=11.,
-                     lon=11.,
-                     depth=10000.,
-                     strike=20.,
-                     dip=40.,
-                     rake=60.,
-                     magnitude=4.)
+    magnitudes = MagnitudeDistribution.GutenbergRichter(a=1, b=1.0)
+    
+    swarm = Swarm(geometry=geometry, 
+                 timing=timing, 
+                 mechanisms=mechanisms,
+                 magnitudes=magnitudes)
 
-# Processing that data will return a pyrocko.gf.seismosizer.Reponse object.
-response = engine.process(sources=[source_dc], 
-                          targets=targets)
+    engine = LocalEngine(store_superdirs=['/data/stores'])
 
-# One way of requesting the processed traces is as follows
-synthetic_traces = response.pyrocko_traces()
+    # The store we are going extract data from:
+    store_id = 'crust2_dd'
+
+    # Processing that data will return a pyrocko.gf.seismosizer.Reponse object.
+    response = engine.process(sources=swarm.get_sources(), 
+                              targets=targets)
+
+    # One way of requesting the processed traces is as follows
+    self.synthetic_traces = response.pyrocko_traces()
+
+    #Visualizer(swarm)
+
 
 
