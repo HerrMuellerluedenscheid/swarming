13e4ed614bdac41684aebe32dcd5db2e31de6ed8
marius <marius.kriegerowski@zmaw.de>
2014-11-19 15:25:23 +0100

    cleanup

--- synthi.py
+++ synthi.py
@@ -1,67 +1,5 @@
 from pyrocko.gf.seismosizer import RemoteEngine, Target
-from pyrocko.gf.seismosizer import DCSource, ExplosionSource
-from pyrocko import trace
 import numpy as num
-
-
-def rot_matrix(theta, phi):
-    theta = theta/180.*num.pi
-    phi = phi/180.*num.pi
-    t = theta
-    p = phi
-    return num.array([[sin(t)*cos(p), cos(t)*cos(p), -sin(p)],
-                     [sin(t)*sin(p), cos(t)*sin(p), cos(p)],
-                     [cos(t), -sin(t), 0.]])
-
-class BaseSourceRegion():
-    def __init__(self, center_lon, center_lat, center_depth, azimuth, dip):
-        self.center_lon = center_lon
-        self.center_lat = center_lat
-        self.center_depth = center_depth
-        self.azimuth = azimuth
-        self.dip = dip
-
-        self.setup()
-    
-    def orientate(self):
-        '''Apply rotation and dipping.'''
-        rm = rot_matrix(self.azimuth, self.dip)
-        return num.dot(self.euler_coords, rm)
-
-    def coordinate(self):
-        self.orientate()
-
-    def setup(self):
-        '''setup x, y and z coordinates of sources'''
-
-        
-class RectrangularSourceRegion(BaseSourceRegion):
-    def __init__(self, top_length, side_length, thickness, *args, **kwargs):
-        BaseSourceRegion.__init__(self, *args, **kwargs)
-
-    def setup(self):
-        z_range = [self.center_depth-side_length/2., 
-                  self.center_depth+side_length/2.]
-        x_range = [-top_length/2., top_length/2.]
-        y_range = [-thickness/2., thickness/2.]
-
-        self.x = num.random.uniform(*x_range)
-        self.z = num.random.uniform(*z_range)
-        self.y = num.random.uniform(*y_range)
-
-
-class swarm():
-    def __init__(self, tmin, tmax):
-        self.tmin = tmin
-        self.tmax = tmax
-        self.geometry = None
-
-    def ellipse(self, a, b, s):
-        return 
-
-    def rectangle(
-
-
 
 # We need a pyrocko.gf.seismosizer.Engine object which provides us with the 
 # traces extracted from the store. In this case we are going to use a local 
@@ -94,7 +32,6 @@ source_dc = DCSource(lat=11.,
 response = engine.process(sources=[source_dc], 
                           targets=targets)
 
-print response
 # One way of requesting the processed traces is as follows
 synthetic_traces = response.pyrocko_traces()
 
