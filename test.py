393885f01dd4fea2adcecd3bf0d6731a5b68dccb
marius <marius.kriegerowski@zmaw.de>
2014-11-19 11:41:53 +0100

    discrete gutenberg richter implementation

--- test.py
+++ test.py
@@ -9,27 +9,53 @@ from scipy import stats
 
 import scipy.stats as st
 
-class my_pdf(st.rv_continuous):
-    def _pdf(self,x):
-        return x*x/10.0
 
-my_cv = my_pdf(a=0.0,b=1.0,name='my_pdf')
-print my_cv.rvs(size=10)
+def GutenbergRichterDiscrete(a,b, Mmin=0., Mmax=8., inc=0.1):
+    """discrete GutenbergRichter randomizer.
+    Use returnvalue.rvs() to draw random number"""
+    x = num.arange(Mmin, Mmax+inc, inc)
+    y = 10**(a-b*x)
+    y/=num.sum(y)
+    randomizer = stats.rv_discrete(name='custm', values=data, inc=inc)
+    return randomizer
+
+class GutenbergRichter(stats.rv_continuous):
+    def _pdf(self, x, aval, bval):
+        return 10**(aval-bval*x)
+
+test = GutenbergRichter(a=0, b=8, name='test', shapes="aval, bval")
+print test
+print 'sadfasdf'
+plt.hist(test.rvs(3,0.5,  size=1000))
+plt.show()
+data = GutenbergRichterDiscrete(0,1)
+print data
+plt.plot(data[0], data[1], 'o')
+plt.show()
+xk = num.arange(7)
+pk = (0.1, 0.2, 0.3, 0.1, 0.1, 0.1, 0.1)
+#h = plt.plot(data[0], custm.pmf(data[0]))
+#plt.figure()
+custm = stats.rv_discrete(name='custm', values=(xk, pk))
+h = plt.plot(xk, custm.pmf(xk))
+plt.figure()
+data =[]
+for i in range(1000):
+    data.append( custm.rvs())
+print data
+plt.hist(data)
+
+plt.show()
 
 
 a = [[1,2,3,4,5,6,7],[7,6,5,4,3,2,1]]
-b = num.array(a).Ty
-c = num.array([[1,2y],[3,4]])
+b = num.array(a).T
+c = num.array([[1,2],[3,4]])
 b = b.reshape(7,2)
-print b
 b = b.T
 i = 0 
 for v in num.nditer(b, op_flags=['readwrite'],flags=['external_loop'], order='C'):
-    print v, i
-    print num.dot(c,v)
     b[...] = num.dot(c,v)
-
 
     i +=1
 
-print b
