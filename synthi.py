79a6ddff62957c543fc00d94e2358ba804d9217f
HerrMuellerluedenscheid <marius.kriegerowski@zmaw.de>
2014-12-14 23:52:25 +0100

    added documentation

--- synthi.py
+++ synthi.py
@@ -34,43 +34,56 @@ if __name__=='__main__':
     stores = os.environ["STORES"]
 
     # Number of sources....
-    number_sources = 20
+    number_sources = 200
 
-    # swarm geometry
-    geometry = RectangularSourceGeometry(center_lon=12.4, 
-                                     center_lat=50.2,
+    # swarm geometry.
+    # center_lat, center_lon and center_depth define the center point of
+    # the desired swarm area.
+    # The orientation is then given as follows.
+    # azimuth is the angle versus north (rotation around z-axis), dip is the
+    # the rotation around the horizontal y axis and tilt around x axis.
+    # length, depth and thickness describe the x-, z- and y-extension
+    geometry = CuboidSourceGeometry(center_lon=12.45, 
+                                     center_lat=50.214,
                                      center_depth=10000,
-                                     azimuth=40.,
+                                     azimuth=-40.,
                                      dip=10.,
-                                     tilt=45.,
+                                     tilt=25.,
                                      length=2000.,
-                                     depth=1000.,
+                                     depth=1500.,
                                      thickness=100., 
                                      n=number_sources)
     
-    # reference source
+    # reference source. All other source mechanisms will be similar to this
+    # one but with some degree of freedom defined by a deviation angle.
+    # I guessed strike dip and rake angles based on the following paper
+    # be Horalek and Sileny:
+    # http://gji.oxfordjournals.org/content/194/2/979.full.pdf?keytype=ref&ijkey=bMwD67zn37OEiJs
     base_source = seismosizer.DCSource(lat=0, lon=0, depth=0, 
-                                       strike=170, dip=80,rake=30)
+                                       strike=170, dip=80,rake=-30)
     
-    # Timing
+    # Timing tmin and tmax are in seconds after 1.1.1970
     timing = RandomTiming(tmin=1000, tmax=100000, number=number_sources)
+
+    # The PropagationTiming class is not finished yet. The idea was to be
+    # able to let the events start nucleating at one point and let them 
+    # propagate through the medium. 
     #timing = PropagationTiming(geometry)
     
     # Focal Mechanisms based on reference source a variation of strike, dip 
     # and rake in degrees and the number of sources.
     mechanisms = FocalDistribution(n=number_sources, 
                                    base_source=base_source, 
-                                   variation=20)
+                                   variation=30)
 
-    # magnitude distribution
-    magnitudes = MagnitudeDistribution.GutenbergRichter(a=1, b=1.0)
+    # magnitude distribution with a- and b- value and a minimum magnitude.
+    magnitudes = MagnitudeDistribution.GutenbergRichter(a=1, b=1.0, Mmin=0.)
 
-    # The store we are going extract green function from:
-    store_id = 'vogtland'
+    # The store we are going extract green functions from:
+    store_id = 'vogtland_50Hz_step'
     engine = LocalEngine(store_superdirs=[stores], 
                          default_store_id=store_id)
 
-    # Obacht: das muss besser!
     store = engine.get_store()
     config = store.config 
     model = config.earthmodel_1d
@@ -87,7 +100,6 @@ if __name__=='__main__':
     stats = load_stations(webnet+'/meta/stations.pf')
     
     # Scrutinize the swarm using matplotlib
-    #Visualizer(swarm, stats)
 
     # convert loaded stations to targets (see function at the top).
     targets = guess_targets_from_stations(stats)
@@ -105,5 +117,4 @@ if __name__=='__main__':
     
     # Save traces:
     io.save(convolved_traces.traces_list(), 'swarm_stf.mseed')
-    # One way of requesting the processed traces is as follows
-    #self.synthetic_traces = response.pyrocko_traces()
+    Visualizer(swarm, stats)
